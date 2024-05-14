03df94eae4227765dbd1b0593e8427c8f1c427cb
i-seesharp <aaranya.barman@mail.utoronto.ca>
2024-05-14 12:52:48 -0700

    create hero title and subtitle in landing page

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,17 @@
+interface MarketingLayoutProps {
+    children: React.ReactNode;
+}
+
+export default function MarketingLayout({
+    children
+}: MarketingLayoutProps) {
+    return (
+        <div className="h-full bg-slate-100">
+            { /* Navbar */ }
+            <main className="pt-40 pb-20 bg-slate-100">
+                { children }
+            </main>
+            { /* Footer */ }
+        </div>
+    );
+}
